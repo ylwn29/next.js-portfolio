a79bb3d87e28fba04a7ae5f9f6597452602537b0
ylwn29 <ylwn29@gmail.com>
2024-05-23 13:53:11 -0700

    modify certifications

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 import TabButton from "./TabButton";
 
 const TAB_DATA = [
@@ -22,7 +23,7 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2 text-xl">
-        <li>Northeastern Univeristy - San Jose, CA</li>
+        <li className="pb-2">Northeastern Univeristy - San Jose, CA</li>
         <li>Rutgers University - Piscataway, NJ</li>
       </ul>
     ),
@@ -32,7 +33,16 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2 text-xl">
-        <li>AWS Cloud Practitioner</li>
+        <li className="pb-2 hover:font-bold">
+          <Link href="https://www.credly.com/badges/4352eb9c-3d29-44f8-819e-272b271b0075/public_url">
+            AWS Cloud Practitioner
+          </Link>         
+        </li>
+        <li className="hover:font-bold">
+          <Link href="https://badgr.com/public/assertions/z6Fbrhf9Q06D5wyZFxKGDQ">
+            Introductory AI Literacy
+          </Link>         
+        </li>
       </ul>
     ),
   },
