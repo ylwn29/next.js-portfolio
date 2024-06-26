de8f39058d451c19b558b66cf13795dfdb690d8e
ylwn29 <ylwn29@gmail.com>
2024-06-26 16:01:22 -0700

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID;
 
 export const metadata = {
   title: 'Yulun Wang',
@@ -11,6 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId={GA_TRACKING_ID} />
       <body className={inter.className}>{children}</body>
     </html>
   )
