ad6dc33b5f262c2ad5cf52d485cbb9b98912e2d3
ylwn29 <ylwn29@gmail.com>
2024-06-26 12:43:27 -0700

    edit resume link

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -43,8 +43,9 @@ const HeroSection = () => {
               Hire Me
             </Link> */}
             <Link
-              href="/"
+              href="https://drive.google.com/file/d/1v5rCHvOcP745HC9hDfzcVpkftezVCGwq/view"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white my-3"
+              target="_blank"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 font-bold">
                 Resume
