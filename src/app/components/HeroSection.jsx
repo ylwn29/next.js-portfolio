f567e5f4284cf7c8da7a56183ea5c2334315d7ff
ylwn29 <ylwn29@gmail.com>
2024-08-08 18:32:18 -0700

    fix motion div

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start lg:px-24 lg:col-span-6"
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start lg:pl-24 lg:col-span-8"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
@@ -57,7 +57,7 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-4 place-self-center mt-4 lg:mt-0 lg:col-span-6"
+          className="col-span-4 place-self-center mt-4 lg:mt-0 lg:col-span-4"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
