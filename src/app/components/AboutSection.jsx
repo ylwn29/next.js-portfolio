5a9b33a9881d7b035149aba440eedf7510fbcebb
ylwn29 <ylwn29@gmail.com>
2024-09-24 16:00:26 -0700

    remove hover on About Me

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -67,7 +67,7 @@ const AboutSection = () => {
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/bg3.jpg" width={650} height={650} />
-        <div className="mt-4 md:mt-0 text-left flex flex-col h-full transition-transform duration-300 hover:scale-110">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4 ">About Me</h2>
           <p className="text-lg lg:text-xl mb-4">
             I am a Master of Science in Computer Science student at Northeastern University. I have experience of Web Development, Cloud Computing, Object-Oriented Design. I am passionate about learning new technologies and am currently exploring the use of OpenAI APIs to deliver diverse functionalities.
